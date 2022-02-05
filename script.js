60b2ca71f6b173591e97301bf09abc8847e2d658
T. K <music@music.ee>
2022-02-05 15:10:36 +0000

    minor bugs fixed

--- script.js
+++ script.js
@@ -2,39 +2,35 @@
 //
 const data = {
   numbers: [0],
-  previous: [],
-  entry: true,
+  previousNum: [],
   negative: false,
   operator: null,
   clickTargetID: null,
 };
 //
-// console.table(data.numbers + "; " + data.numbers.length + "; " + data.numbers[0]);
+function calculate() {}
 //
 function btnEvent(btnValue) {
   if (typeof btnValue === "number") {
-    //   console.table(btnValue + ", number");
     populateDisplayObj(btnValue);
   } else if (btnValue === "\u0043") {
-    // console.table(btnValue + ", clear");
     clearAll();
   } else if (btnValue === "\u00B1") {
-    // console.table(btnValue + ", plusmin");
     data.negative = !data.negative;
     showNumbers();
   } else if (btnValue === "\u002E" && !data.numbers.includes("\u002E")) {
-    //  console.table(btnValue + ", dot");
     populateDisplayObj(btnValue);
   } else {
-    //  console.table(btnValue + ", operator");
+    data.operator = btnValue;
+    console.table(btnValue + ", operator");
   }
 }
 //
 function showNumbers() {
-  const startDiv = 71;
   for (let i = 0; i < 9; i++) {
-    document.getElementById(startDiv - i * 2).textContent =
+    document.getElementById(71 - i * 2).textContent =
       data.numbers[data.numbers.length - 1 - i];
+    console.log(+data.numbers.join("") + " " + typeof +data.numbers.join(""));
   }
   if (data.negative === true) {
     document.getElementById(53).textContent = "-";
@@ -51,14 +47,12 @@ function populateDisplayObj(num) {
   if (data.numbers.length > 9) {
     data.numbers.length = 9;
   }
-  // console.table("cont:" + data.numbers + "; length:" + data.numbers.length);
   showNumbers();
 }
 //
 function clearAll() {
   data.numbers = [0];
-  data.previous = [];
-  data.entry = true;
+  data.previousNum = [];
   data.negative = false;
   data.operator = null;
   showNumbers();
