91888aa98f3351b3fb8047bc8d94599da5026864
T. K <music@music.ee>
2022-02-08 10:21:22 +0000

    Must fix calculations.

--- logic.md
+++ logic.md
@@ -1,26 +1,65 @@
 # Logic
 
-## AC state. num1, num2, op1, op2 empty. [0] on display
+## 1.0 AC state. num1, num2, numRes, op empty. [0] on display
 
-- number: start entering numbers, populate display
+- number: display > num1, displArray cleared, ready entering numbers, populate display
 - decimal: zero is shifted left, decimal on display
 - equals: nothing happens
 - negative: nothing happens
-- operator: operator > op1, ready to enter numbers
+- operator: operator > op, ready to enter numbers
 
-### Non [0] on display
+### 1.1 Non [0] on display
 
-#### difference from AC state
+#### difference from AC state 1.0
 
 - negative: toggles -
 - decimal: enters . if no . on display
 
-## Operator has been pressed first time (op1 exists), no previous calculation
+## 2.0 Operator has been pressed first time (op1 exists), no previous calculation
 
-- number: display > num1, start entering numbers, populate display
-- devimal: display > num1, 0 and decimal on display, populate display
-- equals: display > num1 & num2. Calculate num2=num2;op2;num1. Num2 > display, populate.
+- number: display > num2, displArray cleared, start entering numbers, populate display
+- decimal: display > num2, 0 and decimal on displayArray, ready entering numbers, populate display
+- equals: display > num1 & num2. Calculate: numRes=num2;op;num2. NumRes > display, populate display. Num1 intact.
 - negative: nothing happens
-- operator: replace op2
+- operator: replace op1
 
-## Pr
+## 3.0 Previous calculation exists
+
+- number: displayArray cleared, start entering numbers, populate display
+- decimal: 0 and decimal on displayArray, ready entering numbers, populate display
+- equals: Calculate: numRes=num1;op;num2. NumRes > display, populate. Num2 intact.
+- negative: nothing happens
+- operator: replace op1
+
+## Decimal
+
+- after operator or =: stores display value num, clears display, starts with 0. Display refresh.
+- after a number pushes . if no . on display. Display refresh.
+
+## Number
+
+- after a number pushes number if there is space
+- after operator or =: stores display value num, clears display
+
+## Negative
+
+- toggles state only between number and . entry and operator or 0
+
+## C
+
+- clears all
+
+## Operator
+
+- IF (number, operator, number entered)
+  --triggers calculation
+  -- rewrites op field
+- IF (number entered)
+  --rewrites op field
+
+## Equal
+
+- IF (op empty)
+  -- null
+- ## IF (op exists)
+  -- calculates
--- script.js
+++ script.js
@@ -4,26 +4,28 @@ const data = {
   displayArr: [0],
   num1: null,
   num2: null,
+  numRes: null,
   negative: false,
-  op1: null,
-  op2: null,
-  //  entry: true,
+  op: null,
+  lastKey: null,
   clickTargetID: null,
 };
 //
 function btnEvent(btnValue) {
   if (typeof btnValue === "number") {
     populateDisplayObj(btnValue);
+    data.lastKey = "number";
   } else if (btnValue === "\u0043") {
-    clearAll();
-  } else if (btnValue === "\u00B1") {
-    data.negative = !data.negative;
+    clearAll(); // C
+  } else if (btnValue === "\u00B1" && data.lastKey === "number") {
+    data.negative = !data.negative; //plus-min
     showNumbers();
   } else if (btnValue === "\u002E" && !data.displayArr.includes("\u002E")) {
-    populateDisplayObj(btnValue);
+    populateDisplayObj(btnValue); //decimal
+  } else if (btnValue === "\u003D") {
+    equalBtn(btnValue); //equal
   } else {
-    data.op1 = btnValue;
-    calculate();
+    operatorBtn(btnValue); //one of 4 operators
   }
 }
 //
@@ -54,42 +56,67 @@ function showNumbers() {
   }
 }
 //
-function arrayToDecimal(arr) {
-  return !data.negative ? +arr.join("") : -Math.abs(+arr.join(""));
+function equalBtn(btnValue) {
+  if (!data.numRes && data.lastKey !== "=") {
+    data.numRes = data.num1;
+  }
+  if (data.lastKey !== "=") {
+    data.num1 = displayToNumber();
+  }
+  data.num2 = data.numRes;
+  data.numRes = mathe(data.num2, data.num1, data.op);
+  numberToDisplay();
+  showNumbers();
+  data.displayArr = [];
+  data.lastKey = btnValue;
 }
 //
-function decimalToArray(decimal) {
-  if (decimal < 0) {
+function operatorBtn(btnValue) {
+  if (data.num1 && data.lastKey !== "=") {
+    data.num2 = data.num1;
+  }
+  data.num1 = displayToNumber();
+  if (data.num2 && data.lastKey !== "=") {
+    data.numRes = mathe(data.num2, data.num1, btnValue);
+  }
+  if (data.numRes && data.lastKey !== "=") {
+    numberToDisplay();
+  }
+  showNumbers();
+  data.displayArr = [];
+  data.op = btnValue;
+  data.lastKey = btnValue;
+}
+//
+function displayToNumber() {
+  return !data.negative
+    ? +data.displayArr.join("")
+    : -Math.abs(+data.displayArr.join(""));
+}
+//
+function numberToDisplay() {
+  if (data.numRes < 0) {
     data.negative = true;
   }
-  const arr = Array.from(decimal.toString()).map(Number);
-  for (i = 0; i < arr.length; i++) {
+  const arr = Array.from(Math.abs(data.numRes).toString()).map(Number);
+  for (let i = 0; i < arr.length; i++) {
     if (isNaN(arr[i])) {
       arr[i] = ".";
     }
   }
   data.displayArr = arr;
-  showNumbers();
 }
 //
-function calculate() {
-  if (!data.op2) {
-    //
-  } else {
-    //
-  }
-}
-//
-function mathe(x, y) {
-  switch (data.op2) {
+function mathe(num2, num1, btnValue) {
+  switch (btnValue) {
     case "+":
-      return x + y;
+      return num2 + num1;
     case "-":
-      return x - y;
+      return num2 - num1;
     case "*":
-      return x * y;
+      return num2 * num1;
     case "/":
-      return x / y;
+      return num2 / num1;
     default:
       return;
   }
@@ -99,9 +126,10 @@ function clearAll() {
   data.displayArr = [0];
   data.num1 = null;
   data.num2 = null;
+  data.numRes = null;
   data.negative = false;
-  data.op1 = null;
-  data.op2 = null;
+  data.op = null;
+  data.lastKey = null;
   showNumbers();
 }
 //
